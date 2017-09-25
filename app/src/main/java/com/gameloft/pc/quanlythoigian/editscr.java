3b7521fd88850b8b205410b23fbd1d0473fbe142
smallsparrow97 <baduy9780@gmail.com>
2017-09-25 21:34:16 +0700

    Duy thêm, sửa

--- app/src/main/java/com/gameloft/pc/quanlythoigian/editscr.java
+++ app/src/main/java/com/gameloft/pc/quanlythoigian/editscr.java
@@ -1,6 +1,7 @@
 package com.gameloft.pc.quanlythoigian;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,31 +9,44 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class editscr extends AppCompatActivity {
-
+    private final String SHARED_PREFERENCES_NAME="data_detailscr";
+    private final String MON="mon";
+    private final String PHONG="phong";
+    private final String THOIGIAN="thoigian";
+    private final String GIANGVIEN="giangvien";
+    private final String EMAIL="email";
+    private final String SDT="sdt";
     EditText edtmon,edtphong,edttg,edtgv,edtemail,edtsdt;
-    Button btnluu;
+    Button btnluu,btnxoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editscr);
+
         anhxa();
         btnluu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Bundle bd=new Bundle();
-                bd.putString("mon",edtmon.getText().toString());
-                bd.putString("phong",edtphong.getText().toString());
-                bd.putString("thoigian",edttg.getText().toString());
-                bd.putString("giangvien",edtgv.getText().toString());
-                bd.putString("email",edtemail.getText().toString());
-                bd.putString("sdt",edtsdt.getText().toString());
-                Intent ht=new Intent(editscr.this,detailscr.class);
-                ht.putExtra("hienthi",bd);
+                add_data();
+                Intent ht=new Intent(editscr.this,thoi_khoa_bieu.class);
                 startActivity(ht);
+            }
 
+
+        });
+        btnxoa.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Bundle bd=new Bundle();
+                remove_data();
+                Intent ht1=new Intent(editscr.this,thoi_khoa_bieu.class);
+                startActivity(ht1);
             }
         });
+        inraeditscr();
     }
     private void anhxa(){
         edtmon=(EditText)findViewById(R.id.môn);
@@ -42,5 +56,33 @@ public class editscr extends AppCompatActivity {
         edtemail=(EditText)findViewById(R.id.email);
         edtsdt=(EditText)findViewById(R.id.sdt);
         btnluu=(Button)findViewById(R.id.btnhienthi);
+        btnxoa=(Button)findViewById(R.id.btnxoadata);
+    }
+
+    public void add_data(){
+        SharedPreferences sharedPreferences=getSharedPreferences(SHARED_PREFERENCES_NAME,MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putString(MON,edtmon.getText().toString());
+        editor.putString(PHONG,edtphong.getText().toString());
+        editor.putString(THOIGIAN,edttg.getText().toString());
+        editor.putString(GIANGVIEN,edtgv.getText().toString());
+        editor.putString(EMAIL,edtemail.getText().toString());
+        editor.putString(SDT,edtsdt.getText().toString());
+        editor.apply();
+    }
+    public void inraeditscr(){
+        SharedPreferences sharedPreferences=getSharedPreferences(SHARED_PREFERENCES_NAME,MODE_PRIVATE);
+        edtmon.setText(sharedPreferences.getString(MON,""));
+        edtphong.setText(sharedPreferences.getString(PHONG,""));
+        edttg.setText(sharedPreferences.getString(THOIGIAN,""));
+        edtgv.setText(sharedPreferences.getString(GIANGVIEN,""));
+        edtemail.setText(sharedPreferences.getString(EMAIL,""));
+        edtsdt.setText(sharedPreferences.getString(SDT,""));
+    }
+    public void remove_data(){
+        SharedPreferences sharedPreferences=getSharedPreferences(SHARED_PREFERENCES_NAME,MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
     }
 }
